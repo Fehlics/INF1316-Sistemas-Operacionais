eaf188c687cf5a71d06aa4fded7c47efa9ca02f4
Gabriel Félix de Farias Bereicoa <gabrielbereicoa@aluno.puc-rio.br>
2026-09-26 00:40:59 -0300

    Faz aplicacoes aguardarem a conclusao das syscalls em pipes individuais

--- T1/Application.c
+++ T1/Application.c
@@ -14,12 +14,12 @@
  * IMPORTANTE: nesta etapa a syscall e apenas COMUNICADA, sem bloquear.
  * A conclusao da operacao, o retorno do RECV e o bloqueio virao depois.
  */
-static int solicitar_syscall(int fd, int id, Operacao operacao, int pc, int n) {
+static int solicitar_syscall(int fd, int fd_resposta, int id, Operacao operacao, int pc, int *n) {
     PedidoSyscall pedido = {
         .id_aplicacao = id,
         .operacao = operacao,
         .pc = pc,
-        .n = n
+        .n = *n
     };
 
     ssize_t escritos;
@@ -33,20 +33,43 @@ static int solicitar_syscall(int fd, int id, Operacao operacao, int pc, int n) {
     }
 
     printf("[A%d] Solicitou %s (PC=%d, N=%d)\n", id,
-           operacao == ENVIAR ? "SEND" : "RECV", pc, n);
+           operacao == ENVIAR ? "SEND" : "RECV", pc, *n);
+
+    /* Espera a resposta exclusiva do KernelSim; ainda pode estar sob SIGSTOP. */
+    RespostaSyscall resposta;
+    size_t recebidos = 0;
+    while (recebidos < sizeof resposta) {
+        ssize_t lidos = read(fd_resposta, (char *)&resposta + recebidos,
+                             sizeof resposta - recebidos);
+        if (lidos == 0) return -1;
+        if (lidos < 0) {
+            if (errno == EINTR) continue;
+            perror("Application: read resposta");
+            return -1;
+        }
+        recebidos += (size_t)lidos;
+    }
+    if (resposta.id_aplicacao != id || resposta.operacao != operacao) {
+        fprintf(stderr, "[A%d] Resposta invalida.\n", id);
+        return -1;
+    }
+    if (operacao == RECEBER) *n = resposta.n;
+    printf("[A%d] %s concluido (PC=%d, N=%d)\n",
+           id, operacao == ENVIAR ? "SEND" : "RECV", pc, *n);
     return 0;
 }
 
 int main(int argc, char *argv[]) {
     /* O Simulador fornece o identificador e a ponta de escrita do pipe. */
-    if (argc != 3) {
-        fprintf(stderr, "Uso: Application ID FD_SYSCALL\n");
+    if (argc != 4) {
+        fprintf(stderr, "Uso: Application ID FD_SYSCALL FD_RESPOSTA\n");
         return 1;
     }
 
     int id = atoi(argv[1]);
     int fd_syscall = atoi(argv[2]);
-    if (id < 1 || id > QUANTIDADE_APLICACOES || fd_syscall < 0) {
+    int fd_resposta = atoi(argv[3]);
+    if (id < 1 || id > QUANTIDADE_APLICACOES || fd_syscall < 0 || fd_resposta < 0) {
         fprintf(stderr, "Identificador ou descritor invalido.\n");
         return 1;
     }
@@ -77,8 +100,9 @@ int main(int argc, char *argv[]) {
         }
 
         if (operacao != NENHUMA_OPERACAO &&
-            solicitar_syscall(fd_syscall, id, operacao, pc, n) == -1) {
+            solicitar_syscall(fd_syscall, fd_resposta, id, operacao, pc, &n) == -1) {
             close(fd_syscall);
+            close(fd_resposta);
             return 1;
         }
 
@@ -90,5 +114,6 @@ int main(int argc, char *argv[]) {
     printf("[A%d] Finalizada.\n", id);
     /* TODO: comunicar termino e aguardar conclusao das syscalls futuras. */
     close(fd_syscall);
+    close(fd_resposta);
     return 0;
 }
